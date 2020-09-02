2f349378558082082e4b55e004f1cbf55978d1cf
Evgenia Rangaev <alraner93@gmail.com>
2020-09-01 22:25:26 -0400

    Updates

---
+++ precache-manifest.c9ba49e1d367730c18d9b0c95c9d54cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcb188916873a4cd1a1cf7965ed43b99",
+    "url": "/react-tic-tac-toe_textbook/index.html"
+  },
+  {
+    "revision": "f07d21e94b86c4e9223a",
+    "url": "/react-tic-tac-toe_textbook/static/css/main.08b6adb4.chunk.css"
+  },
+  {
+    "revision": "1017874257c13663a26a",
+    "url": "/react-tic-tac-toe_textbook/static/js/2.c4be1b4a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tic-tac-toe_textbook/static/js/2.c4be1b4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f07d21e94b86c4e9223a",
+    "url": "/react-tic-tac-toe_textbook/static/js/main.2b32cb62.chunk.js"
+  },
+  {
+    "revision": "96217c87d83e0a854ed8",
+    "url": "/react-tic-tac-toe_textbook/static/js/runtime-main.e3ab689d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-tic-tac-toe_textbook/static/media/logo.5d5d9eef.svg"
+  }
+]);
